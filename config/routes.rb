598ca4596a7bb6237975a5b0b1689d39c4738b2c
Kevin Incorvia <kevin@incorvia.me>
2014-02-13 15:13:25 -0500

    rails4 style routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 ApiDoc::Engine.routes.draw do
-  match "*path", to: "documents#show", as: :api_doc_page
+  get "*path", to: "documents#show", as: :api_doc_page
   root to: "documents#index", as: :api_doc_home
 end
